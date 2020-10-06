7ea4cdfa5ea78e9f74f07d467933cfe99ee69841
Chondan <chondansusuwan@outlook.com>
2020-10-06 19:43:34 +0700

    first commit

---
+++ precache-manifest.a998ec25386718d76299b0dc3e956c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4675b91f85440ddf8b14cf30df7340",
+    "url": "/Flashcard-Quiz/index.html"
+  },
+  {
+    "revision": "62a6a8e760ce1b27b872",
+    "url": "/Flashcard-Quiz/static/css/main.cc5b2e3b.chunk.css"
+  },
+  {
+    "revision": "cc6c97ed2a5690a74b66",
+    "url": "/Flashcard-Quiz/static/js/2.876188d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Flashcard-Quiz/static/js/2.876188d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62a6a8e760ce1b27b872",
+    "url": "/Flashcard-Quiz/static/js/main.80d4eaa5.chunk.js"
+  },
+  {
+    "revision": "5a9ec2707da0c3b63360",
+    "url": "/Flashcard-Quiz/static/js/runtime-main.33edead9.js"
+  }
+]);
